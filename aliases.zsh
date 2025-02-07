8b2fb38082853b007bb968a47ada33d483f12544
Yannik Lubas <125293446+yanniklubas@users.noreply.github.com>
2025-02-07 19:35:55 +0100

    feat: add java version aliases

--- aliases.zsh
+++ aliases.zsh
@@ -9,3 +9,6 @@ alias cls="clear"
 
 alias gcfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias vpn="sudo openvpn /etc/openvpn/secloud-vpn.ovpn"
+
+alias java-17="export JAVA_HOME=$(/usr/libexec/java_home -v 17); java -version"
+alias java-8="export JAVA_HOME=$(/usr/libexec/java_home -v 1.8); java -version"
